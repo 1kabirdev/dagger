d19c01bb51043a31b7380fd3b9277591bcb932e8
kabir <kabiragamirzaewv@gmail.com>
2022-07-31 19:25:25 +0300

    add service commit

---
+++ app/src/main/java/com/dagger/api/ApiService.kt
@@ -0,0 +1,26 @@
+package com.dagger.api
+
+import com.dagger.models.Post
+import com.dagger.utils.Constants
+import io.reactivex.Observable
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("posts")
+    fun getPostList(): Observable<List<Post>>
+
+    companion object Factory {
+        fun create(): ApiService {
+            val retrofit = retrofit2.Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Constants.BASE_URL)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
