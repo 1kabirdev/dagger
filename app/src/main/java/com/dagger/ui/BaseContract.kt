cc27a11d1f96d49ac5aff30864d016c726ab1fd2
kabir <kabiragamirzaewv@gmail.com>
2022-07-31 19:28:18 +0300

    base contract commit

--- app/src/main/java/com/dagger/ui/BaseContract.kt
+++ app/src/main/java/com/dagger/ui/BaseContract.kt
@@ -1,4 +1,11 @@
 package com.dagger.ui
 
 class BaseContract {
+    interface Presenter<in T> {
+        fun subscribe()
+        fun unSubscribe()
+        fun attachView(view: T)
+    }
+
+    interface View {}
 }
