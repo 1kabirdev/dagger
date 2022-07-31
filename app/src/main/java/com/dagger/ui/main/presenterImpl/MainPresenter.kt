90a2d0031ce60bea41217f503c18096a4f6ddb8f
kabir <kabiragamirzaewv@gmail.com>
2022-07-31 19:34:18 +0300

    main presenter commit

---
+++ app/src/main/java/com/dagger/ui/main/presenterImpl/MainPresenter.kt
@@ -0,0 +1,41 @@
+package com.dagger.ui.main.presenterImpl
+
+import com.dagger.api.ApiService
+import com.dagger.models.Post
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MainPresenter : MainContract.Presenter {
+
+    private lateinit var view: MainContract.View
+    private var subscription = CompositeDisposable()
+    private var api: ApiService = ApiService.create()
+
+
+    override fun responsePost() {
+        val subscribe = api.getPostList().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ list: List<Post>? ->
+                view.progress(false)
+                view.loadData(list!!.take(10))
+            }, { error ->
+                view.progress(false)
+                view.onError(error.localizedMessage)
+            })
+
+        subscription.add(subscribe)
+    }
+
+    override fun subscribe() {
+
+    }
+
+    override fun unSubscribe() {
+        subscription.clear()
+    }
+
+    override fun attachView(view: MainContract.View) {
+        this.view = view
+    }
+}
