6998df93dc8702b314dea4c1a539189d979703b9
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 14:19:59 +0300

    refactor main constant commit

--- app/src/main/java/com/dagger/ui/main/presenterImpl/MainContract.kt
+++ app/src/main/java/com/dagger/ui/main/presenterImpl/MainContract.kt
@@ -1,5 +1,6 @@
 package com.dagger.ui.main.presenterImpl
 
+import com.dagger.models.DetailsViewModel
 import com.dagger.models.Post
 import com.dagger.ui.BaseContract
 
@@ -9,9 +10,12 @@ interface MainContract {
         fun loadData(list: List<Post>)
         fun progress(show: Boolean)
         fun onError(message: String)
+
+        fun loadDataAllSuccess(model: DetailsViewModel)
     }
 
     interface Presenter : BaseContract.Presenter<View> {
         fun responsePost()
+        fun responseLoadDataAll()
     }
 }
