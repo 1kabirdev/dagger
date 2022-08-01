6751608849511fa13399fdf227b7dcf2b28ac4bf
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 14:19:28 +0300

    refactor presenter rxjava, observable zip, function3 commit

--- app/src/main/java/com/dagger/ui/main/presenterImpl/MainPresenter.kt
+++ app/src/main/java/com/dagger/ui/main/presenterImpl/MainPresenter.kt
@@ -1,7 +1,11 @@
 package com.dagger.ui.main.presenterImpl
 
 import com.dagger.api.ApiService
+import com.dagger.models.Album
+import com.dagger.models.DetailsViewModel
 import com.dagger.models.Post
+import com.dagger.models.User
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -27,8 +31,40 @@ class MainPresenter : MainContract.Presenter {
         subscription.add(subscribe)
     }
 
-    override fun subscribe() {
+    override fun responseLoadDataAll() {
+        val subscribe = Observable.zip(api.getPostList(), api.getUserList(), api.getAlbumList(),
+            object : Function3<List<Post>, List<User>, List<Album>, DetailsViewModel> {
+                override fun invoke(
+                    posts: List<Post>,
+                    users: List<User>,
+                    albums: List<Album>
+                ): DetailsViewModel {
+                    return createDetailsViewModel(posts, users, albums)
+                }
+            }).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ model: DetailsViewModel? ->
+                view.loadDataAllSuccess(model!!)
+            }, { error ->
+                print("ERROR: ${error.localizedMessage}")
+            })
 
+        subscription.add(subscribe)
+    }
+
+
+    private fun createDetailsViewModel(
+        posts: List<Post>,
+        users: List<User>,
+        albums: List<Album>
+    ): DetailsViewModel {
+        val postList = posts.take(30)
+        val userList = users.take(30)
+        val albumList = albums.take(30)
+        return DetailsViewModel(postList, userList, albumList)
+    }
+
+    override fun subscribe() {
     }
 
     override fun unSubscribe() {
