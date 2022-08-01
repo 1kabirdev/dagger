cb073e46a96105dfd410ddaa2b5f113141d4146c
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 14:17:09 +0300

    refactor model commit

--- app/src/main/java/com/dagger/models/Post.kt
+++ app/src/main/java/com/dagger/models/Post.kt
@@ -6,3 +6,15 @@ data class Post(
     val title: String,
     val body: String
 )
+
+data class User(
+    val id: Int,
+    val name: String,
+    val username: String
+)
+
+data class Album(
+    val id: Int,
+    val userId: Int,
+    val title: String
+)
