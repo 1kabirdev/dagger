e7b7265383bfcc20597bc71474e562430176fe8a
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 10:12:06 +0300

    dagger application inject commit

--- app/src/main/java/com/dagger/App.kt
+++ app/src/main/java/com/dagger/App.kt
@@ -2,12 +2,30 @@ package com.dagger
 
 import android.app.Application
 import com.dagger.di.component.ApplicationComponent
+import com.dagger.di.component.DaggerApplicationComponent
+import com.dagger.di.module.ApplicationModule
 
-class App:Application() {
+class App : Application() {
 
     lateinit var component: ApplicationComponent
 
     override fun onCreate() {
         super.onCreate()
+        instance = this
+        setup()
+    }
+
+    private fun setup() {
+        component = DaggerApplicationComponent.builder()
+            .applicationModule(ApplicationModule(this)).build()
+        component.inject(this)
+    }
+
+    fun getApplicationComponent(): ApplicationComponent {
+        return component
+    }
+
+    companion object {
+        lateinit var instance: App private set
     }
 }
