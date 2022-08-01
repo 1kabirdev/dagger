c0d28c49dd8040436c3f55c1851938fd95bb7893
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 14:16:19 +0300

    refactor api commit

--- app/src/main/java/com/dagger/api/ApiService.kt
+++ app/src/main/java/com/dagger/api/ApiService.kt
@@ -1,16 +1,35 @@
 package com.dagger.api
 
+import com.dagger.models.Album
 import com.dagger.models.Post
+import com.dagger.models.User
 import com.dagger.utils.Constants
 import io.reactivex.Observable
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("posts")
     fun getPostList(): Observable<List<Post>>
+
+    @GET("albums")
+    fun getAlbumList(): Observable<List<Album>>
+
+    @GET("albums/{id}")
+    fun getAlbum(@Path("id") id: Int): Observable<Album>
+
+    @GET("posts/{id}")
+    fun getPost(@Path("id") id: Int): Observable<Post>
+
+    @GET("users")
+    fun getUserList(): Observable<List<User>>
+
+    @GET("posts/{id}")
+    fun getUser(@Path("id") id: Int): Observable<User>
+
 
     companion object Factory {
         fun create(): ApiService {
